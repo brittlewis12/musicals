fd668060d89d7b5a22d0216a547a1fb00a0c2095
brittlewis12 <brittlewis12@gmail.com>
2013-10-11 15:58:59 -0400

    small .sql edit

--- broadway.sql
+++ broadway.sql
@@ -1,13 +1,13 @@
 CREATE TABLE shows (
   id serial primary key,
-  title varchar(100),
-  year varchar(4),
-  composer,
-  img_url varchar(255)
+  title varchar(100) NOT NULL,
+  year varchar(4) NOT NULL,
+  composer varchar(100) NOT NULL,
+  img_url varchar(255) NOT NULL
 );
 
 CREATE TABLE songs (
   id serial primary key,
-  embed_url varchar(255),
+  embed_url varchar(255) NOT NULL,
   show_id integer references shows(id)
 );

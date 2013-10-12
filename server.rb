d77afa424d417fe6f04e9a859794aa39854e0e40
brittlewis12 <brittlewis12@gmail.com>
2013-10-12 02:18:13 -0400

    improved display of all shows and all songs as unordered lists. New show form works, and display all/one show works.

--- server.rb
+++ server.rb
@@ -6,78 +6,74 @@ require_relative 'config/environments'
 require_relative 'models/show'
 require_relative 'models/song'
 
+helpers do; end
+
 after do
   ActiveRecord::Base.clear_active_connections!
 end
 
-binding.pry
-
-helpers do; end
-
 # Welcome to Broadway.ly!
-
-get '/' do
+get "/" do
   erb :index
 end
 
 # Index of all shows
 # with links to individual shows
-
-get '/shows' do
+get "/shows/?" do
   @shows = Show.all
 
   erb :shows
 end
 
 # Form to create new show
-
-get '/shows/new' do
+get "/shows/new/?" do
   erb :new_show
 end
 
 # Create action - new show - redirects to that
 # show
-
-post '/shows' do
+post "/shows/?" do
   show = Show.new
   show.title = params[:title]
   show.year = params[:year]
   show.composer = params[:composer]
   show.image_url = params[:image_url]
+
+  # If user input is valid, it is persisted
+  # to the database. Else, user is redirected
+  # back to the form
   if show.valid?
     show.save!
     redirect "shows/#{show.id}"
   else
     # IN FUTURE:
     # Should give user some indication of an error.
-    redirect '/shows/new'
+    redirect "/shows/new"
   end
 end
 
 # Individual show page
 # Links to list of all songs `/shows/:id/songs`
 # and form to create new songs `/shows/:id/songs/new`
+get "/shows/:id/?" do
+  @chosen_show = Show.find(params[:id].to_i)
 
-get '/shows/:id' do
+  erb :single_show
 end
 
 # Form to create new songs
-
-get '/shows/:id/songs/new' do
+get "/shows/:id/songs/new/?" do
 end
 
 # Create action - new songs for a show - redirects
 # to that song
-
-post '/shows/:id/songs' do
+post "/shows/:id/songs/?" do
 end
 
 # Lists all songs from the show
-
-get '/shows/:id/songs' do
+get "/shows/:id/songs/?" do
 end
 
 # Shows just one song from the show
-
-get '/shows/:show_id/songs/:song_id' do
+get "/shows/:show_id/songs/:song_id/?" do
 end
